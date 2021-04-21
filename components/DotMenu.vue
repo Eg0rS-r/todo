9860812db38329ef4d7dd1e5a40cee4a455de9f1
Egor <s4reg0r@gmail.com>
2021-04-21 19:03:54 +0500

    blank porject

---
+++ components/DotMenu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="dot-menu">
+        <button class="dot-menu__button"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            header: 'Menu2'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .dot-menu__button {
+        width: 3px;
+        height: 3px;
+        border-radius: 50%;
+        background-color: black;
+    }
+</style>
